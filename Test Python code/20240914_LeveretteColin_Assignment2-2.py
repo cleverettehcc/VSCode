60739f48427b9d1f4c3dcc5ba8a71ac233cf9bcc
cleverettehcc <cleverette@hawkmail.hccfl.edu>
2024-09-14 16:56:45 -0400

    Assignment 2-2

--- Test Python code/20240914_LeveretteColin_Assignment2-2.py
+++ Test Python code/20240914_LeveretteColin_Assignment2-2.py
@@ -5,6 +5,6 @@
 # Create your variables here
 myCurrentAge = 21
 currentYear = 2024
-myNewAge = myCurrentAge + (2050 - currentYear)
+myNewAge = myCurrentAge + (int(2050) - currentYear)
 print("My Current Age is " + str(myCurrentAge))
 print("I will be " + str(myNewAge) + " in 2050.")
